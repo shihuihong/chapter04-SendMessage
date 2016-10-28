54c8ef0cd953316988a27759ce312b74539c4021
Administrator <Administrator@Sc-201410061308>
2016-10-28 15:01:04 +0800

    消息发送

---
+++ src/com/example/sendmessage/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.sendmessage;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+EditText num,mess;
+Button btn;
+
+public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		btn=(Button)findViewById(R.id.btn);
+		num=(EditText)findViewById(R.id.num);
+		mess=(EditText)findViewById(R.id.mess);
+		btn.setOnClickListener(new OnClickListener(){
+			public void onClick(View v){
+				String mobile=num.getText().toString();
+				String content=mess.getText().toString();
+				Intent intent=new Intent();
+				intent.setData(Uri.parse("smsto:"+mobile));
+				startActivity(intent);
+			}
+			
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
